e7d6e97f462c578d8e86b21474440209a7de167f
Kirill Lysin <klysin@ozon.ru>
2024-11-10 18:46:00 +0300

    add player models

---
+++ Sources/App/DTOs/PlayerDTO.swift
@@ -0,0 +1,35 @@
+import Fluent
+import Vapor
+
+public struct PlayerDTO: Codable {
+    var id: UUID?
+    var userId: UUID?
+    var gameId: UUID?
+    var nickname: String
+    var score: Int;
+    var turnOrder: Int;
+    var availableTiles: String;
+    
+    func toModel() -> Player {
+        let model = Player()
+        
+        if let id = id {
+            model.id = id
+        }
+        
+        if let userId = userId {
+            model.userId = userId
+        }
+        
+        if let gameId = gameId {
+            model.gameId = gameId
+        }
+        
+        model.nickname = nickname
+        model.score = score
+        model.turnOrder = turnOrder
+        model.availableTiles = availableTiles
+        
+        return model
+    }
+}
